9b92f6f5b4054eb3fcd0cca858027d8bd6c94421
William Lowrimore <wlowrimore@gmail.com>
2024-06-15 10:35:07 -0500

    Successfully uploads post to firebase firestore

--- src/app/components/MainPostInput.jsx
+++ src/app/components/MainPostInput.jsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { app } from '../../firebase'
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { addDoc, collection, getFirestore, serverTimestamp } from 'firebase/firestore'
 
 import Image from 'next/image'
 
@@ -13,7 +14,10 @@ const MainPostInput = () => {
   const [imageFileUrl, setImageFileUrl] = useState(null)
   const [selectedFile, setSelectedFile] = useState(null)
   const [imageFileUploading, setImageFileUploading] = useState(false)
+  const [postText, setPostText] = useState('');
+  const [postLoading, setPostLoading] = useState(false);
   const imagePickRef = useRef(null)
+  const db = getFirestore(app);
 
   const addImageToPost = (e) => {
     const file = e.target.files[0];
@@ -57,6 +61,25 @@ const MainPostInput = () => {
     );
   }
 
+  const handleSubmitPost = async () => {
+    setPostLoading(true);
+    const docRef = await addDoc(collection(db, 'posts'), {
+      uid: session.user.uid,
+      name: session.user.name,
+      username: session.user.username,
+      text: postText,
+      profileImg: imageFileUrl,
+      image: imageFileUrl,
+      timestamp: serverTimestamp(),
+    })
+    setPostLoading(false);
+    setPostText('');
+    setSelectedFile(null);
+    setImageFileUrl(null);
+  }
+
+  const loadingText = 'Uploading...';
+
   if (!session) return null
   return (
     <div className='flex border border-gray-200 p-3 space-x-3 w-full'>
@@ -68,15 +91,29 @@ const MainPostInput = () => {
         className='rounded-full w-11 h-11 cursor-pointer hover:brightness-95 transition-all duration-200'
       />
       <div className='w-full divide-y divide-gray-200'>
-        <textarea className='w-full border-none outline-none tracking-wide min-h-[3rem] text-gray-700' rows={2} placeholder={`How can we help, ${session.user.name.split(' ')[0]}?`}></textarea>
+        <textarea
+          className='w-full border-none outline-none tracking-wide min-h-[3rem] text-gray-700'
+          rows={2}
+          placeholder={`How can we help, ${session.user.name.split(' ')[0]}?`}
+          value={postText}
+          onChange={(e) => setPostText(e.target.value)}
+        ></textarea>
 
         {selectedFile && (
-          <img src={imageFileUrl} alt='image' className='w-full max-h-[16rem] object-cover cursor-pointer' />
+          <img
+            src={imageFileUrl}
+            alt='image'
+            className={`w-full max-h-[16rem] object-cover cursor-pointer ${imageFileUploading ? 'animate-pulse' : ''}`} />
         )}
         <div className='flex items-center justify-between pt-2.5'>
           <HiOutlinePhoto onClick={() => imagePickRef.current.click()} className='h-10 w-10 p-2 text-sky-500 hover:bg-sky-100 rounded-full cursor-pointer' />
           <input type='file' ref={imagePickRef} accept='image/*' onChange={addImageToPost} hidden />
-          <button disabled className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'>Post</button>
+          <button
+            disabled={postText.trim() === '' || postLoading || imageFileUploading}
+            className='bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50'
+            onClick={handleSubmitPost}
+          >Post
+          </button>
         </div>
       </div>
     </div>
