b5ed78856cdb04993180dd7bfca5e079f8dfff91
William Lowrimore <wlowrimore@gmail.com>
2024-06-17 22:35:17 -0500

    adds functionality to update post page with comments.

--- src/app/posts/[id]/page.js
+++ src/app/posts/[id]/page.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { app } from '../../../firebase';
+import { getFirestore, doc, getDoc } from 'firebase/firestore'
+import { HiArrowLeft } from 'react-icons/hi'
+import Link from 'next/link'
+import Post from '../../components/Post'
+export default async function PostPage({ params }) {
+  const db = getFirestore(app)
+  let data = {};
+  const querySnapshot = await getDoc(doc(db, 'posts', params.id));
+  data = { ...querySnapshot.data(), id: querySnapshot.id }
 
-const PostPage = () => {
   return (
-    <div>PostPage</div>
+    <div className='max-w-xl mx-auto border-r border-l min-h-screen'>
+      <div className='flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
+        <Link href='/' className='hover:bg-gray-100 rounded-full p-2'><HiArrowLeft className='h-5 w-5' />
+        </Link>
+        <h2 className='sm:text-lg'>Back</h2>
+      </div>
+      <Post post={data} id={data.id} />
+    </div>
   )
 }
-
-export default PostPage
