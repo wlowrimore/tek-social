6df0128d5e975e7c37bfefe16c587cbe53e89711
William Lowrimore <wlowrimore@gmail.com>
2024-07-01 08:39:44 -0500

    corrects logic for saving and retreiving profile data per user

--- src/app/components/UserProfileModal.jsx
+++ src/app/components/UserProfileModal.jsx
@@ -15,6 +15,7 @@ import { getDocs, collection } from 'firebase/firestore';
 import Link from 'next/link';
 import { PiGithubLogoBold, PiLinkedinLogo, PiLetterCirclePBold } from "react-icons/pi";
 import { HiPencil } from 'react-icons/hi';
+
 export default function UserProfileModal() {
   const { data: session } = useSession();
   const uid = session?.user?.uid
@@ -34,25 +35,26 @@ export default function UserProfileModal() {
     const querySnapshot = await getDocs(collection(db, 'profile'));
     const data = [];
     querySnapshot.forEach((doc) => {
-      data.push({ id: doc.uid, profileId: doc.id, ...doc.data() });
+      const profileId = doc.data().profileId
+      data.push({ id: doc.uid, profileId, ...doc.data() });
     })
 
     return data
   }
 
   useEffect(() => {
-    async function fetchProfile(profileId) {
-      if (uid === profileId) {
-        const profile = await fetchProfileFromDB();
-        setProfile(profile[0]);
+    async function fetchProfile() {
+      const profileData = await fetchProfileFromDB();
+      const userProfile = profileData.find(profile => profile.profileId === uid);
+      if (userProfile) {
+        setProfile(userProfile);
       } else {
-        console.log("Not logged in")
+        console.log("User profile not found")
       }
     }
 
     fetchProfile();
-  }, [])
-  console.log("Session Email:", session?.user?.email)
+  }, [uid])
 
   const topFiveSkills = profile?.skills?.slice(0, 5)
 
@@ -65,18 +67,18 @@ export default function UserProfileModal() {
           isOpen={open}
           onRequestClose={() => setOpen(false)}
           ariaHideApp={false}
-          className='max-w-2xl w-[50%] max-h-[80%] absolute top-40 left-[50%] translate-x-[-50%] bg-neutral-50 border-2 border-gray-200 rounded-xl shadow-md outline-none overflow-y-scroll'
+          className='max-w-2xl w-[70%] max-h-[80%] absolute top-40 left-[50%] translate-x-[-50%] bg-neutral-50 border-2 border-gray-200 rounded-xl shadow-md outline-none overflow-y-scroll'
         >
           {session ? (
-            <div className=''>
+            <div className='bg-zinc-100'>
               <HiX onClick={() => setOpen(false)} className='relative left-[93%] top-2 text-2xl text-gray-700 font-bold hover:text-gray-400 transition duration-200 cursor-pointer' />
-              <div className='flex items-start space-x-8 p-4'>
+              <div className='flex items-start space-x-6 p-4'>
                 <div classname='flex flex-col'>
                   <Image
                     src={session?.user?.image || 'No Image Available'}
                     alt='Profile Picture'
-                    width={150}
-                    height={150}
+                    width={175}
+                    height={175}
                     priority
                     className='pt-2 pb-2 px-3 rounded-2xl hover:bg-gray-100 transition duration-200' />
                   <div className='flex flex-col px-3'>
@@ -115,24 +117,24 @@ export default function UserProfileModal() {
                     </div>
                   ) : null}
                   {profile?.gitHubUrl ? (
-                    <div className='flex items-center space-x-2'>
+                    <Link href={profile?.gitHubUrl} target='_blank' rel='noopener noreferrer' className='flex items-center space-x-2 py-2 px-2 hover:bg-zinc-200 rounded-lg transition duration-300 ml-[-0.6rem]'>
                       <h3 className=''><PiGithubLogoBold /></h3>
-                      <p className='text-sm text-gray-600'>{profile.gitHubUrl}</p>
-                    </div>
+                      <p className='text-sm text-gray-600 hover:text-primaryRed'>{profile.gitHubUrl}</p>
+                    </Link>
                   ) : null}
                   {profile?.linkedInUrl ? (
-                    <div className='flex items-center space-x-2'>
+                    <Link href={profile?.linkedInUrl} target='_blank' rel='noopener noreferrer' className='flex items-center space-x-2 py-2 px-2 hover:bg-zinc-200 rounded-lg transition duration-300 ml-[-0.6rem]'>
                       <h3 className=''><PiLinkedinLogo /></h3>
-                      <p className='text-sm text-gray-600'>{profile.linkedInUrl}</p>
-                    </div>
+                      <p className='text-sm text-gray-600 hover:text-primaryRed'>{profile.linkedInUrl}</p>
+                    </Link>
                   ) : null}
                   {profile?.portfolioUrl ? (
-                    <div className='flex items-center space-x-2'>
+                    <Link href={profile?.portfolioUrl} target='_blank' rel='noopener noreferrer' className='flex items-center space-x-2 py-2 px-2 hover:bg-zinc-200 rounded-lg transition duration-300 ml-[-0.6rem]'>
                       <h3 className=''><PiLetterCirclePBold /></h3>
-                      <p className='text-sm text-gray-600'>{profile.portfolioUrl}</p>
-                    </div>
+                      <p className='text-sm text-gray-600 hover:text-primaryRed'>{profile.portfolioUrl}</p>
+                    </Link>
                   ) : null}
-                  <div className='m-4 hover:text-gray-500 transition duration-200 py-1 pr-1'>
+                  <div className='m-4 hover:text-gray-500 transition duration-200 py-1 pr-1 ml-0'>
                     <button onClick={handleDetailsForm} className='flex items-center space-x-2'><p className='text-gray-700 font-semibold hover:text-primaryRed hover:underline transition-all duration-200'>{`${detailsComplete ? 'Edit Profile Details' : 'Add Profile Details'}`}</p><HiPencil /></button>
                   </div>
                 </div>
